402a779ce414baa4839ec9c83228fe384c62ac32
Jasmine Hertiana <jasminehertiana03@gmail.com>
2022-11-12 11:27:46 +0700

    app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const database = require("./config/database");
+const app = express()
+const allRouter = require('./routes')
+// const database = require('./config/database')
+
+const port = 3000;
+
+app.use(express.json())
+app.use(allRouter)
+
+database.then(() => {
+    console.log('connect database successfully !');
+}).catch((error) => {
+    console.log(error);
+})
+
+app.listen(port, () => {
+    console.log('server running at http://localhost:'+ port)
+})
